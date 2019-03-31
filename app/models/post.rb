177130c7db0bbde12df6b402b58fa7ad2cab0d17
Matthew Sherman <msherman0801@gmail.com>
2019-03-31 17:34:02 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
@@ -5,6 +6,7 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
+      binding.pry
       category = Category.find_or_create_by(category_attribute)
       self.post_categories.build(category: category)
     end
